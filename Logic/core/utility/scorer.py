1d170a216b0e0f5f8eeb2743b392649bf7216d34
AmirHossein-nr <arazlighi@gmail.com>
2024-04-26 21:04:20 +0330

    add link_analyzer

--- Logic/core/utility/scorer.py
+++ Logic/core/utility/scorer.py
@@ -1,7 +1,8 @@
 from typing import List
 import numpy as np
 
-class Scorer:    
+
+class Scorer:
     def __init__(self, index, number_of_documents):
         """
         Initializes the Scorer.
@@ -31,14 +32,14 @@ class Scorer:
         -------
         list
             A list of documents that contain at least one of the terms in the query.
-        
+
         Note
         ---------
             The current approach is not optimal but we use it due to the indexing structure of the dict we're using.
             If we had pairs of (document_id, tf) sorted by document_id, we could improve this.
                 We could initialize a list of pointers, each pointing to the first element of each list.
                 Then, we could iterate through the lists in parallel.
-            
+
         """
         list_of_documents = []
         for term in query:
@@ -59,7 +60,7 @@ class Scorer:
         -------
         float
             The inverse document frequency of the term.
-        
+
         Note
         -------
             It was better to store dfs in a separate dict in preprocessing.
@@ -111,7 +112,9 @@ class Scorer:
             for doc_id in self.get_list_of_documents(query)
         }
 
-    def get_vector_space_model_score(self, query, query_tfs, document_id, document_method, query_method):
+    def get_vector_space_model_score(
+        self, query, query_tfs, document_id, document_method, query_method
+    ):
         """
         Returns the Vector Space Model score of a document for a query.
 
@@ -170,7 +173,9 @@ class Scorer:
         return np.dot(query_vectors, doc_vectors)
 
 
-    def compute_socres_with_okapi_bm25(self, query, average_document_field_length, document_lengths):
+    def compute_socres_with_okapi_bm25(
+        self, query, average_document_field_length, document_lengths
+    ):
         """
         compute scores with okapi bm25
 
@@ -183,7 +188,7 @@ class Scorer:
         document_lengths : dict
             A dictionary of the document lengths. The keys are the document IDs, and the values are
             the document's length in that field.
-        
+
         Returns
         -------
         dict
@@ -194,7 +199,9 @@ class Scorer:
             for doc_id in self.get_list_of_documents(query)
         }
 
-    def get_okapi_bm25_score(self, query, document_id, average_document_field_length, document_lengths):
+    def get_okapi_bm25_score(
+        self, query, document_id, average_document_field_length, document_lengths
+    ):
         """
         Returns the Okapi BM25 score of a document for a query.
 
